5c5ae0aa78a4ff978787617681eb9f4fbc2a885b
Thekey213 <azdilinga@gmail.com>
2024-04-18 18:16:20 +0200

    Create the Homepage and added the hero

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DigiImage from '../../assets/images/DigiTech-3.jpg'
+import "./home.css";
+
+
+export default function Home() {
+    return (
+        <>
+        <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#F6EFDF'}}>
+        <img src={DigiImage} className="logo react" alt="React logo" style={{ width: '100px', height: '100px' }} />
+        </div>
+            <section className="hero-bg">
+                <div className="hero-text">
+                    <h1>BUY THE BEST COMPUTERS</h1>
+                    <h2>Shop laptops at a low price</h2>
+                </div>
+            </section>
+
+
+        </>
+    );
+}
